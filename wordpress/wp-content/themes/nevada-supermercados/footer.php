e8707561c1f0955e9acefbab44ba85ad5cf9952c
Marcos Garcia <marckfree@gmail.com>
2013-09-04 09:49:25 -0300

    Adiciona código para tracking via Google Analytics

--- wordpress/wp-content/themes/nevada-supermercados/footer.php
+++ wordpress/wp-content/themes/nevada-supermercados/footer.php
@@ -43,6 +43,16 @@
     </div> <!-- # layout960px -->
 
     <?php wp_footer(); ?>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+    
+    <!-- Google -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-43732021-1', 'nevadasupermercados.com.br');
+      ga('send', 'pageview');
+    </script>
   </body>
 </html>
